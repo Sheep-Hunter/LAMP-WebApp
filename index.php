ea794941bccbe66003b60071e49f802cc5d39b63
Sheep-Hunter <jack.r.folkestone@gmail.com>
2021-10-19 17:03:32 -0400

    lampstuff

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+    <title>JPJ MarketPlace</title>
+    <link href='/style.css'/>
+</head>
+<body>
+    <nav>
+        <b>JPJ MarketPlace</b>
+        <div class="right">
+            <a href="/login.php">Login</a>
+            <a href="/">Home</a>
+            <a href="/signup.php">signup</a>
+        </div>
+
+    </nav>
+    <div class='items'>
+        <div>
+            <a herf="/delllaptop.php"><h1>Dell Laptop</h1></a>
+            <img src="/cat1.jpeg" />
+        </div>
+        <div>
+            <a herf="/cactus.php"><h1>Cactus</h1></a>
+            <img src="/Thinking-of-getting-a-cat.png" />
+        </div>
+    </div>
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,59 @@
+<?php
+   include("config.php");
+   session_start();
+   
+   if($_SERVER["REQUEST_METHOD"] == "POST") {
+      // username and password sent from form 
+      
+      $myusername = mysqli_real_escape_string($db,$_POST['username']);
+      $mypassword = mysqli_real_escape_string($db,$_POST['password']); 
+      
+      $sql = "SELECT id FROM users WHERE username = '$myusername' and password = '$mypassword'";
+      $result = mysqli_query($db,$sql);
+      $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+      $active = $row['active'];
+      
+      $count = mysqli_num_rows($result);
+      
+      // If result matched $myusername and $mypassword, table row must be 1 row
+        
+      if($count == 1) {
+         //session_register("myusername");
+         $_SESSION['login_user'] = $myusername;
+         echo "Success!";
+         header("location: welcome.php");
+      }else {
+         $error = "Your Login Name or Password is invalid";
+      }
+   }
+?>
+<html>
+  <head>
+    <title>Log in</title>
+    <link rel='stylesheet' href='/style.css' />
+  </head>
+  <body>
+    <nav>
+    <b>The Marketplace</b>
+  <div class="right">
+    <a href="/">Home</a> |
+  
+      <a href="/login">Log in</a> |
+      <a href="/signup">Sign up</a>
+    
+  </div>
+</nav>
+
+      <h1 style="text-align: center">Log in</h1>
+      <div id="login">
+         
+        <form method="POST">
+          <input type="text" name="username" placeholder="Username" />
+          <input type="password" name="password" placeholder="Password" />
+          <input type="submit" />
+        </form>
+      
+      </div>
+      
+  </body>
+</html>
