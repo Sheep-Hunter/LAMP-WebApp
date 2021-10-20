e63e78119f2a3048e1f4e48ea6564cd4029fcb09
Sheep-Hunter <jack.r.folkestone@gmail.com>
2021-10-20 06:34:14 -0400

    stuff

--- index.php
+++ index.php
@@ -6,20 +6,23 @@
 </head>
 <body>
     <nav>
-        
+        <b>JPJ MarketPlace</b>
         <div class="right">
-            <b>JPJ MarketPlace</b>
+            
             <a href="/login.php">Login</a>
             <a href="/">Home</a>
             <a href="/signup.php">signup</a>
         </div>
 
     </nav>
+
     <div class='items'>
+
         <div id='item'>
             <a href="/delllaptop.php"><h1>Dell Laptop</h1></a>
             <img src="/images/cat1.jpeg" />
         </div>
+
         <div id='item'>
             <a href="/cactus.php"><h1>Cactus</h1></a>
             <img src="/images/cat1.jpeg" />
