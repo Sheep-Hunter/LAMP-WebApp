03d7018c937d33b6aa6a75987175c3a4461a9e42
Sheep-Hunter <jack.r.folkestone@gmail.com>
2021-10-20 06:07:44 -0400

    welcome

--- welcome.php
+++ welcome.php
@@ -1,3 +1,6 @@
+<?php
+session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,7 +12,7 @@
     </style>
 </head>
 <body>
-    
+    <h1 class="my-5">Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Welcome to our site.</h1>    
     <p>
         <a href="reset-password.php" class="btn btn-warning">Reset Your Password</a>
         <a href="logout.php" class="btn btn-danger ml-3">Sign Out of Your Account</a>
