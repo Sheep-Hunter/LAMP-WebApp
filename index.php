f7a66ec337dec45170bff41bb9cd1aae49c53bfc
Sheep-Hunter <jack.r.folkestone@gmail.com>
2021-10-20 16:09:06 -0400

    a

--- index.php
+++ index.php
@@ -13,6 +13,21 @@
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 		<link href="style2.css" rel="stylesheet" type="text/css">
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+        <style>
+body {margin:25px;}
+
+div.polaroid {
+  width: 80%;
+  background-color: white;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  margin-bottom: 25px;
+}
+
+div.container {
+  text-align: center;
+  padding: 10px 20px;
+}
+</style>
 	</head>
 <body class="loggedin">
     <nav class="navtop">
@@ -35,14 +50,14 @@
 
     <div class='items'>
 
-        <div id='item'>
+        <div class='polaroid'>
             <a href="/delllaptop.php"><h1>Dell Laptop</h1></a>
-            <img src="/images/cat1.jpeg" />
+            <div class='container'><img src="/images/cat1.jpeg" /></div>
         </div>
 
-        <div id='item'>
+        <div class='polaroid'>
             <a href="/cactus.php"><h1>Cactus</h1></a>
-            <img src="/images/cat1.jpeg" />
+            <div class='container'><img src="/images/cat1.jpeg" /></div>
         </div>
     </div>
 </body>
