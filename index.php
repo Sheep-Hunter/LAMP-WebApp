ca68e26ee907e3d6bb9965caab1e235108ffea2b
Sheep-Hunter <jack.r.folkestone@gmail.com>
2021-10-20 16:26:14 -0400

    a

--- index.php
+++ index.php
@@ -13,35 +13,7 @@
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 		<link href="style2.css" rel="stylesheet" type="text/css">
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
-        <style>
-body {margin:25px;}
-
-div.polaroid {
-  width: 50%;
-  background-color: white;
-  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-  margin-bottom: 25px;
-}
-.column {
-  float: left;
-  width: 33.33%;
-  padding: 5px;
-}
-
-/* Clearfix (clear floats) */
-.row::after {
-  content: "";
-  clear: both;
-  display: table;
-}
-
-img {width: 100%}
-
-div.container {
-  text-align: center;
-  padding: 10px 20px;
-}
-</style>
+        
 	</head>
 <body class="loggedin">
     <nav class="navtop">
