594c2079b1d4b38d11b8fc7944e98e5795114ad5
Jess-G95 <79566501+Jess-G95@users.noreply.github.com>
2021-10-20 12:34:20 +0100

    retrieving images from database

--- cactus.php
+++ cactus.php
@@ -2,4 +2,32 @@
     <header>
         Cactus
     </header>
+    <body>
+
+    <table border="2">
+    <tr>
+        <td>Images</td>
+    </tr>
+
+    <?php
+
+    include "config.php"; // Using database connection file here
+
+    $records = mysqli_query($db,"select * from product_images"); // fetch data from database
+
+    while($data = mysqli_fetch_array($records))
+    {
+    ?>
+    <tr>
+        <td><img src="<?php echo $data['images']; ?>" width="100" height="100"></td>
+    </tr>	
+    <?php
+    }
+    ?>
+
+    </table>
+
+    <?php mysqli_close($db);  // close connection ?>
+
+    </body>
 </html>
