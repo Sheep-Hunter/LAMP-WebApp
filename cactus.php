fe58e617df3585f74f8cee86411f121a7a552786
Jess-G95 <79566501+Jess-G95@users.noreply.github.com>
2021-10-20 20:45:05 +0100

    try to show multiple items from category

--- cactus.php
+++ cactus.php
@@ -1,37 +1,3 @@
-<!-- <html>
-    <header>
-        Cactus
-    </header>
-    <body>
-
-    <table border="2">
-    <tr>
-        <td>Images</td>
-    </tr>
-
-    <?php
-
-    // include "config.php"; // Using database connection file here
-
-    // $records = mysqli_query($db,"select * from products WHERE name = 'cat1'"); // fetch data from database
-
-    // while($data = mysqli_fetch_array($records))
-    // {
-    ?>
-    <tr>
-        <td><img src="<?//php echo $data['image']; ?>" width="100" height="100"></td>
-    </tr>	
-    <?php
-    //}
-    ?>
-
-    </table>
-
-    <?php mysqli_close($db);  // close connection ?>
-
-    </body>
-</html> -->
-
 <html>
 <head>
   <title>Cactus Products</title>
@@ -41,15 +7,15 @@
 <table border="2">
   <tr>
     <td>Product</td>
-    <td>Image</td>
     <td>Price</td>
+    <td>Image</td>
   </tr>
 
 <?php
 
 include "config.php"; // Using database connection file here
 
-$records = mysqli_query($db,"select * from products WHERE name = 'cat1'"); // fetch data from database
+$records = mysqli_query($db,"select * from products WHERE category = 'cactus'"); // fetch data from database
 
 while($data = mysqli_fetch_array($records))
 {
