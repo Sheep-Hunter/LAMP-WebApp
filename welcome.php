96f1751aa697a5e8626873128d29cc2ca66dd95c
Sheep-Hunter <jack.r.folkestone@gmail.com>
2021-10-20 15:34:55 -0400

    aa

--- welcome.php
+++ welcome.php
@@ -34,13 +34,6 @@
 		</div>
 	
 </nav>
-<nav>
-    <h1 class="my-5">Hi, <b><?php echo htmlspecialchars($_SESSION["login_user"]); ?></b>. Welcome to our site.</h1>    
-    <p>
 
-        <a href="reset-password.php" class="btn btn-warning">Reset Your Password</a>
-        <a href="logout.php" class="btn btn-danger ml-3">Sign Out of Your Account</a>
-    </p>
-</nav>
 </body>
 </html>
