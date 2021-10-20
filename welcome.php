c1562f4020ae302c84de5dfacd894707c76e1408
Sheep-Hunter <jack.r.folkestone@gmail.com>
2021-10-20 12:13:08 -0400

    type

--- welcome.php
+++ welcome.php
@@ -26,6 +26,15 @@
 </head>
 <body class="loggedin">
 <nav class="navtop">
+
+		<div>
+			<h1>Website Title</h1>
+			<a href="profile.php"><i class="fas fa-user-circle"></i>Profile</a>
+			<a href="logout.php"><i class="fas fa-sign-out-alt"></i>Logout</a>
+		</div>
+	
+</nav>
+<nav>
     <h1 class="my-5">Hi, <b><?php echo htmlspecialchars($_SESSION["login_user"]); ?></b>. Welcome to our site.</h1>    
     <p>
 
