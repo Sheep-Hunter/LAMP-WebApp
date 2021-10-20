e93d217119fe25425328e56de8890f4eb794cf1a
Jess-G95 <79566501+Jess-G95@users.noreply.github.com>
2021-10-20 20:42:43 +0100

    list item details

--- cactus.php
+++ cactus.php
@@ -1,4 +1,4 @@
-<html>
+<!-- <html>
     <header>
         Cactus
     </header>
@@ -11,18 +11,18 @@
 
     <?php
 
-    include "config.php"; // Using database connection file here
+    // include "config.php"; // Using database connection file here
 
-    $records = mysqli_query($db,"select * from products WHERE name = 'cat1'"); // fetch data from database
+    // $records = mysqli_query($db,"select * from products WHERE name = 'cat1'"); // fetch data from database
 
-    while($data = mysqli_fetch_array($records))
-    {
+    // while($data = mysqli_fetch_array($records))
+    // {
     ?>
     <tr>
-        <td><img src="<?php echo $data['image']; ?>" width="100" height="100"></td>
+        <td><img src="<?//php echo $data['image']; ?>" width="100" height="100"></td>
     </tr>	
     <?php
-    }
+    //}
     ?>
 
     </table>
@@ -30,4 +30,42 @@
     <?php mysqli_close($db);  // close connection ?>
 
     </body>
+</html> -->
+
+<html>
+<head>
+  <title>Cactus Products</title>
+</head>
+<body>
+
+<table border="2">
+  <tr>
+    <td>Product</td>
+    <td>Image</td>
+    <td>Price</td>
+  </tr>
+
+<?php
+
+include "config.php"; // Using database connection file here
+
+$records = mysqli_query($db,"select * from products WHERE name = 'cat1'"); // fetch data from database
+
+while($data = mysqli_fetch_array($records))
+{
+?>
+  <tr>
+    <td><?php echo $data['name']; ?></td>
+    <td><?php echo $data['price']; ?></td>
+    <td><img src="<?php echo $data['image']; ?>" width="100" height="100"></td>
+  </tr>	
+<?php
+}
+?>
+
+</table>
+
+<?php mysqli_close($db);  // close connection ?>
+
+</body>
 </html>
