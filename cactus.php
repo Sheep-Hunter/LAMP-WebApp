0e00bb12398e2121e7acd1c001808a159ef72cbc
Jess-G95 <79566501+Jess-G95@users.noreply.github.com>
2021-10-21 12:31:29 +0100

    cart button size

--- cactus.php
+++ cactus.php
@@ -38,7 +38,7 @@ while($data = mysqli_fetch_array($records))
 		<div class="product-tile-footer">
 		<div class="product-title"><?php echo $data["name"]; ?></div>
 		<div class="product-price"><?php echo $data["price"]; ?></div>
-		<div class="cart-action"><input type="text" class="product-quantity" name="quantity" value="1" size="2" /><input type="submit" value="Add to Cart" class="btnAddAction" /></div>
+		<div class="cart-action"><input type="text" class="product-quantity" name="quantity" value="1" size="1" /><input type="submit" value="Add to Cart" class="btnAddAction" /></div>
 		</div>
 		</form>
 	</div>
