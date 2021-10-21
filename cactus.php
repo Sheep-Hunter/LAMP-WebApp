d1fac6ccd976f31b0bddc5a8a4641a3107c50979
Jess-G95 <79566501+Jess-G95@users.noreply.github.com>
2021-10-21 16:31:54 +0100

    category

--- cactus.php
+++ cactus.php
@@ -82,7 +82,7 @@ if(empty($_SESSION["shopping_cart"])) {
                   }
                 ?>
                 <?php
-                $result = mysqli_query($db,"SELECT * FROM products");
+                $result = mysqli_query($db,"SELECT * FROM products WHERE category = 'cactus'");
                 while($row = mysqli_fetch_assoc($result)){
                     echo "<div class='product_wrapper'>
                     <form method='post' action=''>
